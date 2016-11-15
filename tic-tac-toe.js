633163141388e3da35cc1d1fc43876bcd35be03c
Ivan Tovalin <ivantovalin12@gmail.com>
2016-11-15 10:18:14 -0800

    Added board & adjusting circle

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,11 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+   function makeShape() {
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+    circle.setAttribute("cx", 100)
+    circle.setAttribute("cy", 100)
+    circle.setAttribute("r", 50)
+    canvas.appendChild(circle)
+   }
