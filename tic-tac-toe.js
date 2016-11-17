e0af902e2603ca1475368a4b74bc48259b11d0e4
Ivan Tovalin <ivantovalin12@gmail.com>
2016-11-17 10:18:22 -0800

    Added circle to top row of the tic tac toe.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,59 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-var makeShape1 = 0
+  var = "player1"
    function makeShape1() {
      var circle = document.createElementNS(namespace, "circle")
      var canvas = document.getElementById("game-board")
-     circle.setAttribute("cx", 63)
+     circle.setAttribute("cx", 65)
      circle.setAttribute("cy", 50)
      circle.setAttribute("r", 30)
      canvas.appendChild(circle)
-     if (makeShape1 == 0){
-       alert("circle")
-       makeShape1 = 1
+     circle.setAttribute("fill","violet")
+     if (turn == "player1"){
+
+       turn="player2"
      }
    }
+  function makeShape2(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+    circle.setAttribute("cx", 150)
+    circle.setAttribute("cy", 50)
+    circle.setAttribute("r", 30)
+    canvas.appendChild(circle)
+    circle.setAttribute("fill","violet")
+  }
+  function makeShape3(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+    circle.setAttribute("cx", 235)
+    circle.setAttribute("cy", 50)
+    circle.setAttribute("r", 30)
+    canvas.appendChild(circle)
+    circle.setAttribute("fill","violet")
+  }
+  function makeShape4(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+  }
+  function makeShape5(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+  }
+  function makeShape6(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+  }
+  function makeShape7(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+  }
+  function makeShape8(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+  }
+  function makeShape9(){
+    var circle = document.createElementNS(namespace, "circle")
+    var canvas = document.getElementById("game-board")
+  }
