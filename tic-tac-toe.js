fdf6bdedb2e07489291e9cebf9ad9c4017e3188f
Ivan Tovalin <ivantovalin12@gmail.com>
2016-11-18 10:17:41 -0800

    Attempted to change the circle to rectangle for coding

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,9 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-  var = "player1"
+  var turn = "player1"
    function makeShape1() {
+     if (turn == "player1"){
      var circle = document.createElementNS(namespace, "circle")
      var canvas = document.getElementById("game-board")
      circle.setAttribute("cx", 65)
@@ -10,9 +11,16 @@ var namespace = "http://www.w3.org/2000/svg"
      circle.setAttribute("r", 30)
      canvas.appendChild(circle)
      circle.setAttribute("fill","violet")
-     if (turn == "player1"){
-
-       turn="player2"
+     turn ="player2"
+     } else {
+     var rectangle = document.createElementNS(namespace, "rect")
+     var canvas = document.getElementById("game-board")
+     rectangle.setAttribute("x", 50)
+     rectangle.setAttribute("y",50)
+     rectangle.setAttribute("width", 60)
+     rectangle.setAttribute("height", 60)
+     canvas.appendChild(rectangle)
+     turn="player1"
      }
    }
   function makeShape2(){
